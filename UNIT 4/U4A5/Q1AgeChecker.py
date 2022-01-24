fc99e214076815de5df81d0cc191862fbc263c88
CK-OCSB <97174126+CK-OCSB@users.noreply.github.com>
2022-01-24 14:35:03 +0000

    Eclipse push failed, pushed from Relpit t=just to ensure that it is GitHub

---
+++ UNIT 4/U4A5/Q1AgeChecker.py
@@ -0,0 +1,56 @@
+# NAME OF AUTHOR:  CK-OCSB (Carter King)
+# NAME OF THE PROGRAM:  Q1AgeChecker
+# DATE OF CREATION:  21/01/22
+# PURPOSE OF PROGRAM:  To determine what level of schooling the user is in
+
+
+# VARIABLE DEFINITION
+#userAge=int(input())
+
+
+
+# INPUT
+print("Please enter your age :")
+userAge= int(input())
+
+
+# PROCESSING
+if userAge <=(4):
+ print("you are not in school yet!")
+  ########################################## programs determined that the user is not in school yet because the user is 4 years old
+if userAge == (5):
+ print("You must be in elemtary school!")
+if userAge == (6):
+ print("You must be in elemtary school!")
+if userAge == (7):
+ print("You must be in elemtary school!")
+if userAge == (8):
+ print("You must be in elemtary school!")
+if userAge == (9):
+ print("You must be in elemtary school!")
+if userAge == (10):
+ print("You must be in elemtary school!")
+if userAge == (11):
+ print("You must be in elemtary school!")
+###############################################programs determined that the user is in elementary school because they are ages 5-11
+if userAge == (12):
+ print("you must be in intermediate schooling!")
+if userAge == (13):
+ print("you must be in intermediate schooling!")
+if userAge == (14):
+ print("you must be in intermediate schooling!")
+################################################programs determined that the user is in intermediate schooling because they are ages 12-14
+if userAge == (15):
+ print("you must be in high school!")
+if userAge == (16):
+  print("you must be in high school!")
+if userAge == (17):
+  print("you must be in high school!")
+if userAge == (18):
+  print("you must be in high school!")
+###########################################programs determined that the user is in high school because they are ages 15-18
+if userAge >= (19) :
+  print("you must be in collage/university/workforce!")
+#programs determined that the user is in collage/university/workforce because they are 19+
+# OUTPUT
+#The program assumes what level of schooling the user is currently in based on the age they entered.
